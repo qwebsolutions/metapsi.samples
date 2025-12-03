cc3df074544cfce18e67741a0e0912a31d486915
Calin <calin.dobos@qwebsolutions.eu>
2025-12-03 18:34:59 +0200

    ModalControllerPresentEffect

--- Ionic/Program.cs
+++ Ionic/Program.cs
@@ -73,10 +73,6 @@ public static class Program
         b.HeadAppend(b.HtmlScriptModuleReference(new ViewCustomerPage()));
         b.HeadAppend(b.HtmlScriptModuleReference(new EditCustomerModal()));
         b.HeadAppend(b.HtmlTitle("Metapsi CRM"));
-        //b.BodyAppend(
-        //    b.Hyperapp(
-        //        new Model(),
-        //        (b, model) => b.App(model)));
 
         b.BodyAppend(
             b.Hyperapp<object>(
@@ -96,73 +92,4 @@ public static class Program
                         })),
                 (b, model) => b.IonApp(b.IonNav())));
     }
-
-    public static Var<HyperType.Effect> ModalControllerPresentEffect<T>(
-        this SyntaxBuilder b,
-        System.Action<PropsBuilder<ModalOptions>> setOptions,
-        System.Action<SyntaxBuilder, Var<HyperType.Dispatcher>, Var<OverlayEventDetail>> then)
-        where T : ICustomElement, new()
-    {
-        return b.MakeEffect(
-            (b, dispatch) =>
-            {
-                var modalOptions = b.SetProps<ModalOptions>(
-                    b.NewObj(),
-                    b =>
-                    {
-                        b.Set(x => x.component, new T().Tag);
-                        b.AddProps(setOptions);
-                    });
-                var createPromise = b.ModalControllerCreate(modalOptions);
-                b.PromiseThen(createPromise, (SyntaxBuilder b, Var<object> modal) =>
-                {
-                    b.PromiseThen(
-                        b.CallOnObject<Promise>(modal, IonModal.Method.OnDidDismiss),
-                        (SyntaxBuilder b, Var<OverlayEventDetail> result) =>
-                        {
-                            b.Call(then, dispatch, result);
-                        });
-                    b.CallOnObject(modal, IonModal.Method.Present);
-                });
-            });
-    }
-
-    public static Var<HyperType.Effect> ModalControllerPresentEffect<T>(
-        this SyntaxBuilder b,
-        System.Action<PropsBuilder<ModalOptions>> setOptions)
-        where T : ICustomElement, new()
-    {
-        return b.MakeEffect(
-            b =>
-            {
-                b.ModalControllerPresent(
-                    b =>
-                    {
-                        b.Set(x => x.component, new T().Tag);
-                        b.AddProps(setOptions);
-                    });
-            });
-    }
-
-    public static void SetInitProps(this PropsBuilder<ModalOptions> b, IVariable initProps)
-    {
-        var existingProps = b.Get(x => x.componentProps);
-        var propsReference = b.If(
-            b.HasObject(existingProps),
-            b => existingProps,
-            b =>
-            {
-                b.Set(x => x.componentProps, b.NewObj<object>());
-                return b.Get(x => x.componentProps);
-            });
-
-        b.SetProperty(propsReference, "props", initProps);
-    }
-
-    //public static Var<IVNode> App(this LayoutBuilder b, Var<Model> model)
-    //{
-    //    return b.IonApp(
-            
-    //        );
-    //}
 }
